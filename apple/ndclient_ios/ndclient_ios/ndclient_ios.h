f07d365ba16242238c7b4d3001b8b730d963fd47
duanxiuyun <duan@duanxiuyundeMac-mini.local>
2014-08-07 12:00:08 +0800

    init add

---
+++ apple/ndclient_ios/ndclient_ios/ndclient_ios.h
@@ -0,0 +1,13 @@
+//
+//  ndclient_ios.h
+//  ndclient_ios
+//
+//  Created by duanxiuyun on 14-8-7.
+//  Copyright (c) 2014年 duanxiuyun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ndclient_ios : NSObject
+
+@end
---
+++ src/ndclient_ios/ndclient_ios/ndclient_ios.m
@@ -0,0 +1,13 @@
+//
+//  ndclient_ios.m
+//  ndclient_ios
+//
+//  Created by duanxiuyun on 14-8-7.
+//  Copyright (c) 2014年 duanxiuyun. All rights reserved.
+//
+
+#import "ndclient_ios.h"
+
+@implementation ndclient_ios
+
+@end
