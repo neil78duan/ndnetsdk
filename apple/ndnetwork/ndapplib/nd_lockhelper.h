67bac81f4d66a9e86d1afc292f44edbe0816ff50
Neil Duan <luckduan@126.com>
2014-08-23 20:07:32 +0800

    test

---
+++ apple/ndnetwork/ndapplib/nd_lockhelper.h
@@ -0,0 +1,31 @@
+/* file nd_lockhelper.h
+ *
+ * define class of lock helper
+ *
+ * 2010/12/24 14:53:26
+ */
+
+#ifndef _ND_LOCKHELP_H_
+#define _ND_LOCKHELP_H_
+
+
+//#include "nd_common/nd_common.h"
+#include "ndapplib/nd_object.h"
+class LockHelper
+{
+public:
+	LockHelper(nd_mutex *lock)
+	{
+		NDCallTrace name("nd_mutex_lock") ;
+		m_lock = lock ;
+		nd_mutex_lock(lock) ;
+	}
+	~LockHelper() 
+	{
+		nd_mutex_unlock(m_lock) ;
+	}
+private:
+	nd_mutex *m_lock ; 
+};
+
+#endif 
