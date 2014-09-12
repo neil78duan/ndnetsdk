09683d0f0ab76940e18066dc91a192f4e716c1ba
duanxiuyun <duan@duanxiuyundeMac-mini.local>
2014-09-12 17:10:54 +0800

    add config

---
+++ config.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+#  config.sh
+#  config for ndsdk
+#
+#  Created by duan .
+#
+
+workDir=`pwd`
+
+
+create_ndhome() {
+    cd $HOME
+    if [ -f .bash_profile ]; then
+        echo "export NDHOME=\"$HOME/Documents/ndsdk.github\"" >> .bash_profile
+    elif [ -f .profile ]; then
+        echo "export NDHOME=\"$HOME/Documents/ndsdk.github\"" >> .profile
+    else
+        echo "could not create NDHOME "
+        echo "PLEASE set evn $NDHOME "
+        cd $workDir
+        exit 1
+    fi
+    cd $workDir
+}
+
+# create env
+
+if [ "x$NDHOME" == "x" ]; then
+    create_ndhome
+fi
+
+# create output dir
+PLATFORM_BITS=`getconf LONG_BIT`
+
+if [ $PLATFORM_BITS -eq 64 ]; then
+    [ -d bin64 ] || mkdir bin64
+    [ -d lib64 ] || mkdir lib64
+else
+    [ -d bin ] || mkdir bin
+    [ -d lib ] || mkdir lib
+fi
+
+echo "config SUCCESS "
+
+if [ "x$NDHOME" == "x" ]; then
+    echo "You need relogin and run make to install"
+else
+    echo "Run make to install"
+fi
+
+
