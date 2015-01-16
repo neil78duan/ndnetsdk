8daaeb4af8079ecf7a39555ef933862fc936b694
duanxiuyun <duan@duanxiuyundeMac-mini-3.local>
2015-01-16 10:39:39 +0800

    version

---
+++ src/ndapplib/nd_version.cpp
@@ -0,0 +1,12 @@
+//
+//  nd_version.cpp
+//  ndMacStatic
+//
+//  Created by duanxiuyun on 15-1-15.
+//  Copyright (c) 2015年 duanxiuyun. All rights reserved.
+//
+
+#include <stdio.h>
+
+const char *__g_version_desc ="version 1.0 \n build time: \n" ;
+const int __g_version_id = 100000 ;
