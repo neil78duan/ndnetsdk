35b6c1764cc63624549415778b51e3c7f657d741
duanxiuyun <duan@duanxiuyundeMac-mini-3.local>
2015-02-06 13:07:10 +0800

    add debug/release for makefile

--- run-srv.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-ARCH_MACHINE=`uname -m`
-OS_kernel=`uname -s |  tr '[A-Z]' '[a-z]'`
-
-WORKDIR="./bin/"$OS_kernel"_"$ARCH_MACHINE
-
-
-$WORKDIR/srvDemo -f  ./cfg/config.xml -c test_srv_config
