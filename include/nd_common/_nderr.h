28b1a7d34b447f0a4af4834b40979ba9714f5207
neil78duan <luckduan@126.com>
2015-05-23 15:31:30 +0800

    change encode

--- include/nd_common/_nderr.h
+++ include/nd_common/_nderr.h
@@ -1,9 +1,9 @@
-//
+﻿//
 //  _nderr.h
 //  Define error-code number
 //
 //  Created by duanxiuyun on 14-12-19.
-//  Copyright (c) 2014年 duanxiuyun. All rights reserved.
+//  Copyright (c) 2014 duanxiuyun. All rights reserved.
 //
 #ifdef _MSC_VER
 #pragma  warning(disable: 4819)
@@ -11,24 +11,24 @@
 
 //
 
-ErrorElement(NDERR_INVALID_HANDLE),	//Œﬁ–ßæ‰±˙
-ErrorElement(NDERR_TIMEOUT)   ,		//≥¨ ±
-ErrorElement(NDERR_NOSOURCE) ,		//√ª”–◊„πª◊ ‘¥
-ErrorElement(NDERR_OPENFILE),			//≤ªƒ‹¥Úø™Œƒº˛
-ErrorElement(NDERR_BADTHREAD),		//≤ªƒ‹¥Úø™œﬂ≥Ã
-ErrorElement(NDERR_LIMITED),			//◊ ‘¥≥¨π˝…œœﬁ
-ErrorElement(NDERR_USER),				//¥¶¿Ì”√ªß ˝æ›≥ˆ¥Ì(œ˚œ¢ªÿµ˜∫Ø ˝∑µªÿ-1
-ErrorElement(NDERR_INVALID_INPUT) ,	//Œﬁ–ßµƒ ‰»Î(DATA IS TO BIG OR ZERO
+ErrorElement(NDERR_INVALID_HANDLE),	//无效句柄
+ErrorElement(NDERR_TIMEOUT)   ,		//超时
+ErrorElement(NDERR_NOSOURCE) ,		//没有足够资源
+ErrorElement(NDERR_OPENFILE),			//不能打开文件
+ErrorElement(NDERR_BADTHREAD),		//线程错误
+ErrorElement(NDERR_LIMITED),			//限制使用
+ErrorElement(NDERR_USER),				//没用用户
+ErrorElement(NDERR_INVALID_INPUT) ,	//无效的输入(DATA IS TO BIG OR ZERO
 ErrorElement(NDERR_IO)		,		//IO bad SYSTEM IO BAD
-ErrorElement(NDERR_WUOLD_BLOCK) ,		//–Ë“™◊Ë»˚	
+ErrorElement(NDERR_WUOLD_BLOCK) ,		//需要阻塞	
 ErrorElement(NDERR_CLOSED),			//socket closed by peer
-ErrorElement(NDERR_BADPACKET)  ,		//Õ¯¬Á ‰»Î ˝æ›¥ÌŒÛ(too long or short)
-ErrorElement(NDERR_BADSOCKET) ,		//Œﬁ–ßµƒsocket
+ErrorElement(NDERR_BADPACKET)  ,		//封包错误(too long or short)
+ErrorElement(NDERR_BADSOCKET) ,		//无效的socket
 ErrorElement(NDERR_READ),				//read error
 ErrorElement(NDERR_WRITE),			//write error	
-ErrorElement(NDERR_NO_PRIVILAGE),		//√ª”–»®œﬁ
-ErrorElement(NDERR_RESET),			//±ª÷ÿ÷√
-ErrorElement(NDERR_USER_BREAK),		//”√ªß÷–∂œ(ÕÀ≥ˆ—≠ª∑)
-ErrorElement(NDERR_VERSION),			//∞Ê±æ∫≈¥ÌŒÛ
+ErrorElement(NDERR_NO_PRIVILAGE),		//没有权限
+ErrorElement(NDERR_RESET),			//被重置
+ErrorElement(NDERR_USER_BREAK),		//用户中断
+ErrorElement(NDERR_VERSION),			//版本错误
 ErrorElement(NDERR_UNHANDLED_MSG),			//unknow message
 ErrorElement(NDERR_UNKNOW)			//unknowwing error
