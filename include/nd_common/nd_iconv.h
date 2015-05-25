57dd73c506ceb2f56ba37b7ea16b2de97363702a
neil78duan <luckduan@126.com>
2015-05-25 14:40:11 +0800

    add iconv

---
+++ include/nd_common/nd_iconv.h
@@ -0,0 +1,17 @@
+//
+//  nd_iconv.h
+//  gameHall
+//
+//  Created by duanxiuyun on 15-5-11.
+//  Copyright (c) 2015ๅนด duanxiuyun. All rights reserved.
+//
+
+#ifndef __gameHall__nd_iconv__
+#define __gameHall__nd_iconv__
+
+#include <stdio.h>
+
+#include "nd_common/nd_common.h"
+CPPAPI char * nd_gbk_to_utf8(const char *input_text, char *output_buf, int size_buf);
+CPPAPI char * nd_utf8_to_gbk(const char *input_text, char *output_buf, int size_buf);
+#endif /* defined(__gameHall__nd_iconv__) */
