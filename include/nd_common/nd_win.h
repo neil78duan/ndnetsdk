e3b7699feb6d20caa8499b01f681e52dcc0f57ae
neil78duan <luckduan@126.com>
2015-07-27 17:03:21 +0800

    fix winsock include error

--- include/nd_common/nd_win.h
+++ include/nd_common/nd_win.h
@@ -24,17 +24,36 @@
 // #endif
 // #endif
 
-#ifndef _UNNEED_INCLUDE_WINDOWS_SYSTEM
+// 
+// #ifndef _UNNEED_INCLUDE_WINDOWS_SYSTEM
+// 
+// #if defined(_WINSOCK2API_) || defined(_WINSOCKAPI_)
+// #else 
+// #include <winsock2.h>
+// #endif
+// 
+// #ifndef _WINDOWS_
+// #include <windows.h>
+// #endif
+// #endif
 
-#if defined(_WINSOCK2API_) || defined(_WINSOCKAPI_)
-#else 
+#ifndef _WINSOCK2API_
+
+// Prevent inclusion of winsock.h
+#ifdef _WINSOCKAPI_
+#error Header winsock.h is included unexpectedly.
+#endif
+
+// NOTE: If you use Windows Platform SDK, you should enable following definition:
+// #define USING_WIN_PSDK
+
+#if !defined(WIN32_LEAN_AND_MEAN) && (_WIN32_WINNT >= 0x0400) && !defined(USING_WIN_PSDK)
+#include <windows.h>
+#else
 #include <winsock2.h>
 #endif
 
-#ifndef _WINDOWS_
-#include <windows.h>
-#endif
-#endif
+#endif//_WINSOCK2API_
 
 #include <stdlib.h>
 #include <stdio.h>
