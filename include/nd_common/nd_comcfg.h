f1b623f1b680cf15f59888a87719c09bbef98d32
Neil Duan <luckduan@126.com>
2016-02-04 17:16:10 +0800

    compile config

--- include/nd_common/nd_comcfg.h
+++ include/nd_common/nd_comcfg.h
@@ -23,7 +23,7 @@
 #define ND_ENCODE_TYPE E_SRC_CODE_UTF_8
 #endif
 
-#if defined(ND_ANDROID)
+#if defined(_GUI_TOOL_)
 
 //#define ND_OPEN_TRACE		1
 #define ND_OPEN_LOG_COMMON 	1
@@ -33,86 +33,86 @@
 #define ND_OPEN_LOG_FATAL	1
 //#define ND_SOURCE_TRACE	1
 //#define ND_FILE_TRACE		1
-#define ND_OUT_LOG_2CTRL	1
-#define ND_OUT_LOG_2FILE	1
+//#define ND_OUT_LOG_2CTRL	1
+//#define ND_OUT_LOG_2FILE	1
 //#define ND_MEM_CHECK		1
 #define ND_USE_MSGBOX		1
-#define ND_LOG_WITH_SOURCE	1
+//#define ND_LOG_WITH_SOURCE	1
 #define ND_LOG_WITH_TIME	1
 //#define ND_MEM_STATICS	1
 //#define ND_UNUSE_STDC_ALLOC 1
 #else 
 
 
-//#define ND_USE_GPERF		1		//使用GOOGLE的内存优化库
+//#define ND_USE_GPERF		1		//使锟斤拷GOOGLE锟斤拷锟节达拷锟脚伙拷锟斤拷
 
 #ifdef ND_DEBUG
 
 #ifdef _MSC_VER
-//#define ND_USE_VLD		1		//使用VDL查找内存泄露
+//#define ND_USE_VLD		1		//使锟斤拷VDL锟斤拷锟斤拷锟节达拷泄露
 #endif
 
 #define ND_OVER_RIDE_NEW	1
 
 #if defined(ND_USE_GPERF )||defined(ND_USE_VLD)
 #else
-//#define ND_UNUSE_STDC_ALLOC 1		//不使用标准C的alloc
-//#define ND_MEM_CHECK		1		//内存访问越界检查
-//#define ND_MEM_STATICS		1		//内测申请统计
-//#define ND_SOURCE_TRACE		1		//记录每个申请的资源(打开的文件和申请的内存,还有系统资源)
-//#define ND_FILE_TRACE		1		//记录每个打开的文件
+//#define ND_UNUSE_STDC_ALLOC 1		//锟斤拷使锟矫憋拷准C锟斤拷alloc
+//#define ND_MEM_CHECK		1		//锟节达拷锟斤拷锟斤拷越锟斤拷锟斤拷锟斤拷
+//#define ND_MEM_STATICS		1		//锟节诧拷锟斤拷锟斤拷统锟斤拷
+//#define ND_SOURCE_TRACE		1		//锟斤拷录每锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷源(锟津开碉拷锟侥硷拷锟斤拷锟斤拷锟斤拷锟斤拷锟节达拷,锟斤拷锟斤拷系统锟斤拷源)
+//#define ND_FILE_TRACE		1		//锟斤拷录每锟斤拷锟津开碉拷锟侥硷拷
 #endif
 
-//#define ND_OPEN_TRACE 		1		//打开VC调试窗口输出功能
-#define ND_OPEN_LOG_COMMON 	1		//打开正常log输出
-#define ND_OPEN_LOG_DEBUG	1		//打开debug log输出
-#define ND_OPEN_LOG_WARN	1		//打开警告 log
-#define ND_OPEN_LOG_ERROR	1		//打开错误log
-#define ND_OPEN_LOG_FATAL	1		//打开严重错误输出
-#define ND_OUT_LOG_2CTRL	1		//把日志输出到控制台
-#define ND_OUT_LOG_2FILE	1		//把日志输出到文件
-#define ND_USE_MSGBOX		1		//使用messagebox
-#define ND_LOG_WITH_SOURCE	1		//日志输出带文件名
-#define ND_LOG_WITH_TIME	1		//日志输出带时间
+//#define ND_OPEN_TRACE 		1		//锟斤拷锟斤拷VC锟斤拷锟皆达拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷
+#define ND_OPEN_LOG_COMMON 	1		//锟斤拷锟斤拷锟斤拷锟斤拷log锟斤拷锟斤拷
+#define ND_OPEN_LOG_DEBUG	1		//锟斤拷锟斤拷debug log锟斤拷锟斤拷
+#define ND_OPEN_LOG_WARN	1		//锟津开撅拷锟斤拷 log
+#define ND_OPEN_LOG_ERROR	1		//锟津开达拷锟斤拷log
+#define ND_OPEN_LOG_FATAL	1		//锟斤拷锟斤拷锟斤拷锟截达拷锟斤拷锟斤拷锟斤拷
+#define ND_OUT_LOG_2CTRL	1		//锟斤拷锟斤拷志锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷台
+#define ND_OUT_LOG_2FILE	1		//锟斤拷锟斤拷志锟斤拷锟斤拷锟斤拷锟侥硷拷
+#define ND_USE_MSGBOX		1		//使锟斤拷messagebox
+#define ND_LOG_WITH_SOURCE	1		//锟斤拷志锟斤拷锟斤拷锟斤拷锟侥硷拷锟斤拷
+#define ND_LOG_WITH_TIME	1		//锟斤拷志锟斤拷锟斤拷锟斤拷时锟斤拷
 #else
 
-//#define ND_USE_GPERF		1		//使用GOOGLE的内存优化库
+//#define ND_USE_GPERF		1		//使锟斤拷GOOGLE锟斤拷锟节达拷锟脚伙拷锟斤拷
 #if defined(ND_USE_GPERF )||defined(ND_USE_VLD)
 #else
-//#define ND_UNUSE_STDC_ALLOC 1		//不使用标准C的alloc
-//#define ND_MEM_CHECK		1		//内存访问越界检查
-//#define ND_MEM_STATICS		1		//内测申请统计
+//#define ND_UNUSE_STDC_ALLOC 1		//锟斤拷使锟矫憋拷准C锟斤拷alloc
+//#define ND_MEM_CHECK		1		//锟节达拷锟斤拷锟斤拷越锟斤拷锟斤拷锟斤拷
+//#define ND_MEM_STATICS		1		//锟节诧拷锟斤拷锟斤拷统锟斤拷
 #endif
-//#define ND_OPEN_TRACE		1		//打开VC调试窗口输出功能
-#define ND_OPEN_LOG_COMMON 	1		//打开正常log输出
-//#define ND_OPEN_LOG_DEBUG	1		//打开debug log输出
-//#define ND_OPEN_LOG_WARN	1		//打开警告 log
-#define ND_OPEN_LOG_ERROR	1		//打开错误log
-#define ND_OPEN_LOG_FATAL	1		//打开严重错误输出
-//#define ND_SOURCE_TRACE	1		//记录每个申请的资源(打开的文件和申请的内存,还有系统资源)
-//#define ND_FILE_TRACE		1		//记录每个打开的文件
-#define ND_OUT_LOG_2CTRL	1		//把日志输出到控制台
-#define ND_OUT_LOG_2FILE	1		//把日志输出到文件
-//#define ND_MEM_CHECK		1		//内存访问越界检查
-#define ND_USE_MSGBOX		1		//使用messagebox
-#define ND_LOG_WITH_SOURCE	1		//日志输出带文件名
-#define ND_LOG_WITH_TIME	1		//日志输出带时间
-//#define ND_MEM_STATICS		1		//内测申请统计
+//#define ND_OPEN_TRACE		1		//锟斤拷锟斤拷VC锟斤拷锟皆达拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷
+#define ND_OPEN_LOG_COMMON 	1		//锟斤拷锟斤拷锟斤拷锟斤拷log锟斤拷锟斤拷
+//#define ND_OPEN_LOG_DEBUG	1		//锟斤拷锟斤拷debug log锟斤拷锟斤拷
+//#define ND_OPEN_LOG_WARN	1		//锟津开撅拷锟斤拷 log
+#define ND_OPEN_LOG_ERROR	1		//锟津开达拷锟斤拷log
+#define ND_OPEN_LOG_FATAL	1		//锟斤拷锟斤拷锟斤拷锟截达拷锟斤拷锟斤拷锟斤拷
+//#define ND_SOURCE_TRACE	1		//锟斤拷录每锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷源(锟津开碉拷锟侥硷拷锟斤拷锟斤拷锟斤拷锟斤拷锟节达拷,锟斤拷锟斤拷系统锟斤拷源)
+//#define ND_FILE_TRACE		1		//锟斤拷录每锟斤拷锟津开碉拷锟侥硷拷
+#define ND_OUT_LOG_2CTRL	1		//锟斤拷锟斤拷志锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷台
+#define ND_OUT_LOG_2FILE	1		//锟斤拷锟斤拷志锟斤拷锟斤拷锟斤拷锟侥硷拷
+//#define ND_MEM_CHECK		1		//锟节达拷锟斤拷锟斤拷越锟斤拷锟斤拷锟斤拷
+#define ND_USE_MSGBOX		1		//使锟斤拷messagebox
+#define ND_LOG_WITH_SOURCE	1		//锟斤拷志锟斤拷锟斤拷锟斤拷锟侥硷拷锟斤拷
+#define ND_LOG_WITH_TIME	1		//锟斤拷志锟斤拷锟斤拷锟斤拷时锟斤拷
+//#define ND_MEM_STATICS		1		//锟节诧拷锟斤拷锟斤拷统锟斤拷
 
 #endif			//end debug
 #endif 			//ANDROID
 
-#define ND_BUFSIZE 4096						//默认缓冲大小(临时缓冲,不要修改)
+#define ND_BUFSIZE 4096						//默锟较伙拷锟斤拷锟斤拷小(锟斤拷时锟斤拷锟斤拷,锟斤拷要锟睫革拷)
 
 #define ND_FILE_PATH_SIZE	256
 
-#define NOT_SUPPORT_THIS_FUNCTION 0			//在assert中提示不支持的功能
+#define NOT_SUPPORT_THIS_FUNCTION 0			//锟斤拷assert锟斤拷锟斤拷示锟斤拷支锟街的癸拷锟斤拷
 
-#define ND_MULTI_THREADED 1				//使用多线程(必须)
+#define ND_MULTI_THREADED 1				//使锟矫讹拷锟竭筹拷(锟斤拷锟斤拷)
 
 #define ND_MAX_THREAD_NUM 16
 
-#define ND_CALLSTACK_TRACE	1			//跟踪函数调用堆栈
+#define ND_CALLSTACK_TRACE	1			//锟斤拷锟劫猴拷锟斤拷锟斤拷锟矫讹拷栈
 
 #ifndef BUILD_AS_STATIC_LIB
 	#define BUILD_AS_STATIC_LIB
@@ -120,10 +120,10 @@
 // 
 // #ifdef BUILD_AS_STATIC_LIB
 // #else
-// #define ND_COMPILE_AS_DLL	1			//编译成动态库
+// #define ND_COMPILE_AS_DLL	1			//锟斤拷锟斤拷锟缴讹拷态锟斤拷
 // #endif
 
-//不支持unicode
+//锟斤拷支锟斤拷unicode
 //#define ND_UNICODE			1
 
 //without iconv
