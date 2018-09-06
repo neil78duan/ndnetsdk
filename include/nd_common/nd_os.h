1de7c39caf2cb4de024a057bf0278e6a318fc880
neil78duan <luckduan@126.com>
2018-09-06 18:28:26 +0800

    change common project setting

--- include/nd_common/nd_os.h
+++ include/nd_common/nd_os.h
@@ -27,6 +27,22 @@
 #define ndsnprintf 		snprintf
 #define ndstrncmp		strncmp
 
+#define ndstrcat	strcat			//把一个字符串接到另一个字符串的尾部 
+#define ndstrncat	strncat			//而且指定粘接字符串的粘接长度. 
+#define ndstrchr	strchr			//查找子字符串的第一个位置 
+#define ndstrrchr	strrchr			//从尾部开始查找子字符串出现的第一个位置 
+#define ndstrpbrk	strpbrk			//从一字符字符串中查找另一字符串中任何一个字符第一次出现的位置 
+#define ndstrstr	strstr			//在一字符串中查找另一字符串第一次出现的位置 
+#define ndstrcspn	strcspn			//返回不包含第二个字符串的的初始数目 
+#define ndstrspn	strspn			//返回包含第二个字符串的初始数目 
+#define ndstrcpy	strcpy			//拷贝字符串 
+#define ndstrncpy	strncpy			//同时指定拷贝的数目 
+#define ndstrcmp	strcmp			//比较两个宽字符串 
+#define ndstrncmp	strncmp			//指定比较字符字符串的数目 
+#define ndstrlen	strlen			// 获得宽字符串的数目 
+#define ndstrtok	strtok
+
+
 #if !defined(ND_UNIX) 
 #include "nd_common/nd_win.h"
 
--- src/nd_vm/src/nd_vm.c
+++ src/nd_vm/src/nd_vm.c
@@ -7,6 +7,19 @@
 #include "nd_vm/nd_vm.h"
 
 #include <math.h>
+#include <string.h>
+
+#ifndef __cplusplus
+
+#ifndef max 
+#define max(a,b) 			(((a)>(b))? (a) : (b))
+#endif 
+
+#ifndef min
+#define min(a,b) 			(((a)>(b))? (b) : (a))
+#endif
+
+#endif 
 
 /*
 	÷∏¡ÓΩ·ππ :
