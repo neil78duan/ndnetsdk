b363cb22b7e43702b5844cb4293ed58449a77483
neil78duan <luckduan@126.com>
2019-09-04 15:22:52 +0800

    add var type

---
+++ include/ndapplib/nd_vartype.h
@@ -0,0 +1,102 @@
+/* file NDVarType.h
+ *
+ * define variable type for net message 
+ *
+ * create by duan
+ */
+
+#ifndef _ND_VAR_TYPE_H_
+#define _ND_VAR_TYPE_H_
+
+#include "nd_common/nd_export_def.h"
+#include "nd_common/nd_define.h"
+
+#include <string>
+
+struct ndvtype_bin
+{
+	size_t size;
+	char data[0];
+};
+
+union ndvtype_data{
+	int i_val;
+	float f_val;
+	NDUINT64 i64_val;
+	char *str_val;
+	ndvtype_bin *bin_val;
+};
+
+class ND_COMMON_CLASS NDVarType
+{
+
+public:
+	enum NDVTYPE_ELEMENT_TYPE
+	{
+		ND_VT_INT = 0x0,
+		ND_VT_FLOAT = 0x1,
+		ND_VT_STRING = 0x2,
+		ND_VT_INT8 = 0x3,
+		ND_VT_INT16 = 0x4,
+		ND_VT_INT64 = 0x5,
+		ND_VT_BINARY = 0x7
+	};
+
+	NDVarType();
+	virtual ~NDVarType();
+
+	//init
+	//init set vale
+	NDVarType(int a);
+	NDVarType(NDUINT8 a);
+	NDVarType(NDUINT16 a);
+	NDVarType(NDUINT64 a);
+	NDVarType(bool a);
+	NDVarType(float a);
+	NDVarType(const char* text);
+	NDVarType(void *bindata, size_t size);
+
+	NDVTYPE_ELEMENT_TYPE getDataType() { return m_type; }
+
+	NDVarType &operator =(int a);
+	NDVarType &operator =(NDUINT8 a);
+	NDVarType &operator =(NDUINT16 a);
+	NDVarType &operator =(NDUINT64 a);
+	NDVarType &operator =(bool a);
+	NDVarType &operator =(float a);
+	NDVarType &operator =(const char* text);
+	NDVarType &operator =(const NDVarType&r);
+	
+	NDVarType  operator+(const NDVarType &r) const;
+	NDVarType  operator-(const NDVarType &r) const;
+	NDVarType  operator*(const NDVarType &r) const;
+	NDVarType  operator/(const NDVarType &r) const;
+
+	NDVarType &operator+=(const NDVarType &r);
+	NDVarType &operator-=(const NDVarType &r) ;
+	NDVarType &operator*=(const NDVarType &r) ;
+	NDVarType &operator/=(const NDVarType &r) ;
+
+	NDVarType  operator+(const char *text) const;
+	NDVarType &operator+=(const char *text);
+
+
+	int getInt()const;
+	NDUINT8 getInt8()const;
+	NDUINT16 getInt16()const;
+	NDUINT64 getInt64()const;
+	bool getBool()const;
+	float getFloat()const;
+	const char *getText()const;
+	std::string getString()const;
+	void *getBin()const;
+	size_t getBinSize()const;
+protected:
+	bool initSet(const char*);
+	bool initSet(void *bindata, size_t size);
+	void destroy();
+	NDVTYPE_ELEMENT_TYPE m_type;
+	ndvtype_data m_data;
+};
+
+#endif
---
+++ src/ndapplib/nd_vartype.cpp
@@ -0,0 +1,445 @@
+/* file NDVarType.cpp
+ *
+ * define variable type for net message 
+ *
+ * create by duan
+ */
+
+#include "ndapplib/nd_vartype.h"
+#include "nd_common/nd_str.h"
+#include <stdlib.h>
+
+NDVarType::NDVarType() : m_type(ND_VT_INT)
+{
+	m_data.i64_val = 0;
+}
+NDVarType::~NDVarType()
+{
+	destroy();
+}
+//init
+//init set vale
+NDVarType::NDVarType(int a)
+{
+	m_type = ND_VT_INT;
+	m_data.i_val = a;
+}
+NDVarType::NDVarType(NDUINT8 a)
+{
+	m_type = ND_VT_INT8;
+	m_data.i_val = a;
+}
+NDVarType::NDVarType(NDUINT16 a)
+{
+	m_type = ND_VT_INT16;
+	m_data.i_val = a;
+}
+NDVarType::NDVarType(NDUINT64 a)
+{
+	m_type = ND_VT_INT64;
+	m_data.i64_val = a;
+}
+NDVarType::NDVarType(float a)
+{
+	m_type = ND_VT_FLOAT;
+	m_data.f_val = a;
+}
+
+NDVarType::NDVarType(bool a)
+{
+	m_type = ND_VT_INT;
+	m_data.i_val = a?1:0;
+}
+
+NDVarType::NDVarType(const char* text)
+{
+	initSet(text);
+}
+
+NDVarType::NDVarType(void *bindata, size_t size)
+{
+	initSet(bindata, size);
+}
+
+
+void NDVarType::destroy()
+{
+	if (m_type == ND_VT_STRING || m_type == ND_VT_BINARY) {
+		if (m_data.str_val) { free(m_data.str_val); }		
+	}
+	m_data.str_val = 0;
+}
+
+bool NDVarType::initSet(const char*text)
+{
+	destroy();
+	m_type = ND_VT_STRING;
+	if (text && *text) {
+		size_t size = strlen(text) + 1;
+		m_data.str_val = (char*)malloc(size);
+		if (m_data.str_val) {
+			strncpy(m_data.str_val, text, size);
+			return true;
+		}
+		return false;
+	}
+	return true;
+}
+
+bool NDVarType::initSet(void *bindata, size_t size)
+{
+	destroy();
+	m_type = ND_VT_BINARY;
+	if (size > 0) {
+		m_data.bin_val = (ndvtype_bin *)malloc(size + sizeof(ndvtype_bin) + 4);
+		if (m_data.bin_val) {
+			memcpy(m_data.bin_val->data, bindata, size);
+			m_data.bin_val->size = size;
+			return true;
+		}
+		return false;
+	}
+	return true;
+}
+
+NDVarType &NDVarType::operator =(int a)
+{
+	destroy();
+	m_type = ND_VT_INT;
+	m_data.i_val = a;
+	return *this;
+}
+NDVarType &NDVarType::operator =(NDUINT8 a)
+{
+	destroy();
+	m_type = ND_VT_INT8;
+	m_data.i_val = a;
+	return *this;
+}
+NDVarType &NDVarType::operator =(NDUINT16 a)
+{
+	destroy();
+	m_type = ND_VT_INT16;
+	m_data.i_val = a;
+	return *this;
+}
+NDVarType &NDVarType::operator =(NDUINT64 a)
+{
+	destroy();
+	m_type = ND_VT_INT64;
+	m_data.i64_val = a;
+	return *this;
+}
+NDVarType &NDVarType::operator =(bool a)
+{
+	destroy();
+	m_type = ND_VT_INT;
+	m_data.i_val = a?1:0;
+	return *this;
+}
+NDVarType &NDVarType::operator =(float a)
+{
+	destroy();
+	m_type = ND_VT_FLOAT;
+	m_data.f_val = a;
+	return *this;
+}
+NDVarType &NDVarType::operator =(const char* text)
+{
+	destroy();
+	initSet(text);
+	return *this;
+}
+NDVarType &NDVarType::operator =(const NDVarType&r)
+{
+	destroy();
+	switch (r.m_type)
+	{
+	case NDVarType::ND_VT_INT:
+		*this = r.m_data.i_val;
+		break;
+	case NDVarType::ND_VT_FLOAT:
+		*this = r.m_data.f_val;
+		break;
+	case NDVarType::ND_VT_STRING:
+		initSet(r.m_data.str_val);
+		break;
+	case NDVarType::ND_VT_INT8:
+		*this = r.getInt8();
+		break;
+	case NDVarType::ND_VT_INT16:
+		*this = r.getInt16();
+		break;
+	case NDVarType::ND_VT_INT64:
+		*this = r.getInt64();
+		break;
+	case NDVarType::ND_VT_BINARY:
+		initSet(m_data.bin_val, m_data.bin_val->size);
+		break;
+	default:
+		break;
+	}
+	return *this;
+}
+
+
+int NDVarType::getInt()const
+{
+	if (ND_VT_FLOAT == m_type) {
+		return (int)m_data.f_val;
+	}
+	else if (m_type == ND_VT_STRING) {
+		if (m_data.str_val && m_data.str_val[0]) {
+			return atoi(m_data.str_val);
+		}
+		else {
+			return 0;
+		}
+	}
+	else {
+		return m_data.i_val;
+	}
+}
+
+NDUINT8 NDVarType::getInt8()const
+{
+	return (NDUINT8)getInt();
+}
+NDUINT16 NDVarType::getInt16()const
+{
+	return (NDUINT16)getInt();
+}
+NDUINT64 NDVarType::getInt64()const
+{
+	if (ND_VT_FLOAT == m_type) {
+		return (NDUINT64)m_data.f_val;
+	}
+	else if (m_type == ND_VT_STRING) {
+		if (m_data.str_val && m_data.str_val[0]) {
+			return atoll(m_data.str_val);
+		}
+		else {
+			return 0;
+		}
+	}
+	else {
+		return m_data.i64_val;
+	}
+}
+
+bool NDVarType::getBool()const
+{
+	if (m_type == ND_VT_STRING) {
+		if (m_data.str_val && m_data.str_val[0]) {
+			if (0 == ndstricmp(m_data.str_val, "yes") || 0 == ndstricmp(m_data.str_val, "true")) {
+				return true;
+			}
+		}
+		return false;
+		
+	}
+	else {
+		return getInt()?true:false;
+	}
+}
+
+float NDVarType::getFloat()const
+{
+	if (ND_VT_FLOAT == m_type) {
+		return m_data.f_val;
+	}
+	else if (m_type == ND_VT_STRING) {
+		if (m_data.str_val && m_data.str_val[0]) {
+			return (float)atof(m_data.str_val);
+		}
+		else {
+			return 0;
+		}
+	}
+	else {
+		return (float)m_data.i64_val;
+	}
+}
+const char *NDVarType::getText()const
+{
+	if (m_type == ND_VT_STRING) {
+		return m_data.str_val;
+	}
+	else {
+		return NULL;
+	}
+}
+std::string NDVarType::getString()const
+{
+	char tmpbuf[32];
+	std::string retval;
+	switch (m_type)
+	{
+	case NDVarType::ND_VT_INT:
+		snprintf(tmpbuf, sizeof(tmpbuf), "%d", m_data.i_val);
+		retval = tmpbuf;
+		break;
+	case NDVarType::ND_VT_FLOAT:
+		snprintf(tmpbuf, sizeof(tmpbuf), "%f", m_data.f_val);
+		retval = tmpbuf;
+		break;
+	case NDVarType::ND_VT_STRING:
+		if (m_data.str_val) {
+			retval = m_data.str_val;
+		}
+		break;
+	case NDVarType::ND_VT_INT8:
+		snprintf(tmpbuf, sizeof(tmpbuf), "%d", (NDUINT8)m_data.i_val);
+		retval = tmpbuf;
+		break;
+	case NDVarType::ND_VT_INT16:
+		snprintf(tmpbuf, sizeof(tmpbuf), "%d", (NDUINT16)m_data.i_val);
+		retval = tmpbuf;
+		break;
+	case NDVarType::ND_VT_INT64:
+		snprintf(tmpbuf, sizeof(tmpbuf), "%lld", m_data.i64_val);
+		retval = tmpbuf;
+		break;
+	case NDVarType::ND_VT_BINARY:
+		if (m_data.bin_val && m_data.bin_val->size) {
+			retval.insert(0, m_data.bin_val->data, m_data.bin_val->size);
+		}
+		break;
+	default:
+		break;
+	}
+	return retval;
+}
+void *NDVarType::getBin()const
+{
+	if (m_type == NDVarType::ND_VT_BINARY && m_data.bin_val) {
+		return m_data.bin_val->data;
+	}
+	return NULL;
+}
+size_t NDVarType::getBinSize()const
+{
+
+	if (m_type == NDVarType::ND_VT_BINARY && m_data.bin_val) {
+		return m_data.bin_val->size;
+	}
+	return 0;
+}
+
+//OPERATE
+
+#define ND_VARDATA_MATH_OP(r,_OP) \
+	switch (m_type)	{				\
+	case ND_VT_INT8:						\
+	case ND_VT_INT16:						\
+	case ND_VT_INT:						\
+	{									\
+		int val = getInt() _OP r.getInt();	\
+		return NDVarType(val);			\
+	}				\
+	case ND_VT_INT64:	\
+	{				\
+		NDUINT64 val = getInt64() _OP r.getInt64();	\
+		return NDVarType(val);					\
+	}				\
+	case ND_VT_FLOAT:	\
+	{				\
+		float val = getFloat() _OP r.getFloat();	\
+		return NDVarType(val);				\
+	}				\
+	default:							\
+		break;							\
+	}
+
+
+NDVarType  NDVarType::operator+(const NDVarType &r) const
+{
+	ND_VARDATA_MATH_OP(r, +);
+	return *this;
+}
+NDVarType  NDVarType::operator-(const NDVarType &r) const
+{
+	ND_VARDATA_MATH_OP(r, -);
+	return *this;
+}
+NDVarType  NDVarType::operator*(const NDVarType &r) const
+{
+	ND_VARDATA_MATH_OP(r, *);
+	return *this;
+}
+NDVarType  NDVarType::operator/(const NDVarType &r) const
+{
+	float f1 = getFloat();
+	float f2 = r.getFloat();
+	float val = 0;
+	if (f2 == 0) {
+		val = f1;
+	}
+	else {
+		val = f1 / f2;
+	}
+
+	switch (m_type)
+	{
+	case ND_VT_INT8:
+		return NDVarType((NDUINT8)val);
+	case ND_VT_INT16:
+		return NDVarType((NDUINT16)val);
+	case ND_VT_INT:
+		return NDVarType((int)val);
+	case ND_VT_INT64:
+		return NDVarType((NDUINT64)val);
+	case ND_VT_FLOAT:
+		return NDVarType(val);
+	default:
+		break;
+	}
+	return *this;
+}
+
+NDVarType &NDVarType::operator+=(const NDVarType &r)
+{
+	NDVarType val = *this;
+	*this = val + r;
+	return *this;
+}
+NDVarType &NDVarType::operator-=(const NDVarType &r)
+{
+	NDVarType val = *this;
+	*this = val - r;
+	return *this;
+}
+NDVarType &NDVarType::operator*=(const NDVarType &r)
+{
+	NDVarType val = *this;
+	*this = val * r;
+	return *this;
+}
+NDVarType &NDVarType::operator/=(const NDVarType &r)
+{
+	NDVarType val = *this;
+	*this = val / r;
+	return *this;
+}
+
+NDVarType  NDVarType::operator+(const char *text) const
+{
+	if (m_type == ND_VT_STRING) {
+		std::string str1;
+		if (m_data.str_val && *m_data.str_val) {
+			str1 = m_data.str_val;
+		}
+		if (text && text) {
+			str1 += text;
+		}
+		return NDVarType(str1.c_str());
+	}
+	return NDVarType(text);
+}
+
+NDVarType &NDVarType::operator+=(const char *text)
+{
+	NDVarType val = *this;
+	*this = val + text;
+	return *this;
+}
